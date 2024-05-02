9031035d215e791c88e245dd732054eb8ef49f4d
Valentina-Maraio <valentina.maraio89@gmail.com>
2024-05-02 10:11:50 +0200

    face recognition in images

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import matplotlib.pyplot as plt
+
+imagePath = 'input_image.jpg'
+img = cv2.imread(imagePath)
+print(img.shape)
+#(604, 448, 3)
+
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+print(gray_image.shape)
+#(604, 448)
+
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascades_frontalface_default.xml"
+)
+face = face_classifier.detectMultiScale(
+    gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+)
+for (x, y, w, h) in face:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(20,10))
+plt.imshow(img_rgb)
+plt.axis('off')
+plt.show()
